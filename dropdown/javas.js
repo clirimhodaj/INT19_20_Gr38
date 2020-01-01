82b0ba52c11df71e26013702f0d49396e015952c
Çlirim <53019723+Limi210@users.noreply.github.com>
2020-01-01 23:44:48 +0100

    Add files via upload

---
+++ dropdown/javas.js
@@ -0,0 +1,20 @@
+function changeimg(){
+document.getElementById('tv').src="tv1.jpg";
+}
+
+function backToNormal(){
+  document.getElementById('tv').src="tv.jpg";
+}
+
+function ndryshotel(){
+  document.getElementById('tel').src="samsung.jpg";
+}
+function rikthe(){
+  document.getElementById('tel').src="pexels-photo-270686.jpeg";
+}
+function ndryshopc() {
+  document.getElementById('pc').src="66701442_1209588519165529_2707558812306374656_n.jpg";
+}
+function back() {
+  document.getElementById('pc').src="pexels-photo.jpg";
+}
