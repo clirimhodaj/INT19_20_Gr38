93fad44415dea344b65060bfe02172465a3e5b6b
aureldemiraj <58442040+aureldemiraj@users.noreply.github.com>
2020-01-04 17:40:00 +0100

    Create contactus.js

---
+++ page5/js/contactus.js
@@ -0,0 +1,59 @@
+$(document).ready(function(){
+  			$("#btn1").click(function(){
+   			 $("#Outer1").toggle(500);
+  });
+});
+
+ $(document).ready(function(){
+  			$("#btn2").click(function(){
+   			 $("#Outer2").toggle(500);
+  });
+});	
+
+ $(document).ready(function(){
+  			$("#btn3").click(function(){
+   			 $("#Outer3").toggle(500);
+  });
+});	
+
+  $(document).ready(function(){
+  			$("#btn4").click(function(){
+   			 $("#Outer4").toggle(500);
+  });
+});	
+
+   $(document).ready(function(){
+  			$("#btn5").click(function(){
+   			 $("#Outer5").toggle(500);
+  });
+});	
+
+$(document).ready(function(){
+        $("#btn1").blur(function(){
+         $("#Outer1").toggle();
+  });
+});
+
+$(document).ready(function(){
+        $("#btn2").blur(function(){
+         $("#Outer2").toggle();
+  });
+});
+
+$(document).ready(function(){
+        $("#btn3").blur(function(){
+         $("#Outer3").toggle();
+  });
+});
+
+$(document).ready(function(){
+        $("#btn4").blur(function(){
+         $("#Outer4").toggle();
+  });
+});
+
+$(document).ready(function(){
+        $("#btn5").blur(function(){
+         $("#Outer5").toggle();
+  });
+});
