839947d1147c57aa7557f5dd86b752186c899c6e
Çlirim <53019723+Limi210@users.noreply.github.com>
2020-01-05 23:15:32 +0100

    Update javas.js

--- dropdown/javas.js
+++ dropdown/javas.js
@@ -18,3 +18,54 @@ function ndryshopc() {
 function back() {
   document.getElementById('pc').src="pexels-photo.jpg";
 }
+
+function ndryshohome(){
+document.getElementById('s1').innerHTML="Servisimi";
+}
+
+
+function pop() {
+    window.alert("Te dhenat jane proceduar");
+}
+
+
+$(".fa-facebook-f").mouseenter(function(){
+  var divi=$(".fa-facebook-f");
+  divi.animate({opacity:'0.4'},"fast");
+});
+$(".fa-facebook-f").mouseout(function(){
+  var divi=$(".fa-facebook-f");
+  divi.animate({opacity:'1'},"fast");
+});
+$(".fa-twitter").mouseenter(function(){
+  var divi=$(".fa-twitter");
+  divi.animate({opacity:'0.4'},"fast");
+});
+$(".fa-twitter").mouseout(function(){
+  var divi=$(".fa-twitter");
+  divi.animate({opacity:'1'},"fast");
+});
+$(".fa-instagram").mouseenter(function(){
+  var divi=$(".fa-instagram");
+  divi.animate({opacity:'0.4'},"fast");
+});
+$(".fa-instagram").mouseout(function(){
+  var divi=$(".fa-instagram");
+  divi.animate({opacity:'1'},"fast");
+});
+
+
+function validateForm() {
+  var x = document.forms["formaIme"]["name"].value;
+  if (x == "") {
+    alert("Emri nuk duhet te jete i zbrazet");
+
+  }else if(c==0){
+    document.getElementById("warning").style.display="block";
+    c=1;
+  }
+  else {
+    document.getElementById("warning").style.display="none";
+    c=0;
+  }
+}
