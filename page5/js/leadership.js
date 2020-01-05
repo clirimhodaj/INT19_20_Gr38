337d944adcf067d9f7809a8a7476acb396a57627
aureldemiraj <58442040+aureldemiraj@users.noreply.github.com>
2020-01-05 20:58:35 +0100

    Create leadership.js

---
+++ page5/js/leadership.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+ 
+
+  			$("#btn1").click(function(){
+   			 $("#Outer1").fadeToggle(100);
+  });
+});
+
+ $(document).ready(function(){
+  			$("#btn2").click(function(){
+   			 $("#Outer2").fadeToggle(100);
+  });
+});	
+
+$(document).ready(function(){
+  			$("#btn1").blur(function(){
+   			 $("#Outer1").fadeToggle(100);
+  });
+});
+
+ $(document).ready(function(){
+  			$("#btn2").blur(function(){
+   			 $("#Outer2").fadeToggle(100);
+  });
+});	
